2d10caf68180b9b3cc3b9598ea03a87c48e04e6f
Hyan Cortez da Silveira <hyancsilveira@gmail.com>
2024-11-30 15:47:00 -0300

    Seção 04 - EF Core - Comandos

--- FuscaFilmes.API/DbContexts/Context.cs
+++ FuscaFilmes.API/DbContexts/Context.cs
@@ -9,4 +9,12 @@ public class Context (DbContextOptions<Context> options) : DbContext (options)
 public DbSet<Filme> Filmes { get; set; }
 public DbSet<Diretor> Diretores { get; set; }
     // protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseSqlite ("");
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Diretor>()
+        .HasMany(e => e.Filmes)
+        .WithOne(e => e.Diretor)
+        .HasForeignKey(e => e.DiretorId)
+        .IsRequired();
+    }
 }
