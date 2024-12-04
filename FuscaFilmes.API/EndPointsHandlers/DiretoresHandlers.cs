439508ae310c97a659b0a829696cae474769680c
Hyan Cortez da Silveira <hyancsilveira@gmail.com>
2024-12-04 08:08:00 -0300

    Seção 8 - Métodos Async

--- FuscaFilmes.API/EndPointsHandlers/DiretoresHandlers.cs
+++ FuscaFilmes.API/EndPointsHandlers/DiretoresHandlers.cs
@@ -5,36 +5,36 @@ namespace FuscaFilmes.API.EndPointsHandlers;
 
 public static class DiretoresHandlers
 {
-    public static IEnumerable<Diretor> GetDiretores(IDiretorRepository diretorRepository)
+    public static async Task<IEnumerable<Diretor>> GetDiretoresAsync(IDiretorRepository diretorRepository)
     {
-        return diretorRepository.GetDiretores();
+        return await diretorRepository.GetDiretoresAsync();
     }
 
-    public static Diretor GetDiretorByName(IDiretorRepository diretorRepository, string name)
+    public static async Task <Diretor> GetDiretorByNameAsync(IDiretorRepository diretorRepository, string name)
     {
-        return diretorRepository.GetDiretorByName(name);
+        return await diretorRepository.GetDiretorByNameAsync(name);
     }
-    public static IEnumerable<Diretor> GetDiretoresById(IDiretorRepository diretorRepository, int id)
+    public static async Task<IEnumerable<Diretor>> GetDiretoresByIdAsync(IDiretorRepository diretorRepository, int id)
     {
-        return diretorRepository.GetDiretoresById(id);
+        return await diretorRepository.GetDiretoresByIdAsync(id);
     }
-    public static void AddDiretor(IDiretorRepository diretorRepository, Diretor diretor)
+    public static async Task AddDiretorAsync(IDiretorRepository diretorRepository, Diretor diretor)
     {
-        diretorRepository.Add(diretor);
-        diretorRepository.SaveChanges();
-    }
-
-    public static void UpdateDiretor(IDiretorRepository diretorRepository, Diretor diretorNovo)
-    {
-
-        diretorRepository.Update(diretorNovo);
-        diretorRepository.SaveChanges();
+        await diretorRepository.AddAsync(diretor);
+        await diretorRepository.SaveChangesAsync();
     }
 
-    public static void DeleteDiretor(IDiretorRepository diretorRepository, int diretorId)
+    public static async Task UpdateDiretorAsync(IDiretorRepository diretorRepository, Diretor diretorNovo)
     {
-        diretorRepository.Delete(diretorId);
-        diretorRepository.SaveChanges();
+
+        await diretorRepository.UpdateAsync(diretorNovo);
+        await diretorRepository.SaveChangesAsync();
+    }
+
+    public static async Task DeleteDiretorAsync(IDiretorRepository diretorRepository, int diretorId)
+    {
+        await diretorRepository.DeleteAsync(diretorId);
+        await diretorRepository.SaveChangesAsync();
     }
 }
 
--- FuscaFilmes.API/EndpointsExtensions/EndpointDiretores.cs
+++ FuscaFilmes.API/EndpointsExtensions/EndpointDiretores.cs
@@ -9,16 +9,16 @@ public static class EndpointDiretores
 
     public static void DiretoresEndpoints(this IEndpointRouteBuilder app)
     {
-        app.MapGet("/diretores", DiretoresHandlers.GetDiretores).WithOpenApi();
+        app.MapGet("/diretores", DiretoresHandlers.GetDiretoresAsync).WithOpenApi();
 
-        app.MapGet("/diretores/agregacao{name}", DiretoresHandlers.GetDiretorByName).WithOpenApi();
+        app.MapGet("/diretores/agregacao{name}", DiretoresHandlers.GetDiretorByNameAsync).WithOpenApi();
 
-        app.MapGet("/diretores/where{id}", DiretoresHandlers.GetDiretoresById).WithOpenApi();
+        app.MapGet("/diretores/where{id}", DiretoresHandlers.GetDiretoresByIdAsync).WithOpenApi();
 
-        app.MapPost("/diretores", DiretoresHandlers.AddDiretor).WithOpenApi();
+        app.MapPost("/diretores", DiretoresHandlers.AddDiretorAsync).WithOpenApi();
 
-        app.MapPut("/diretores", DiretoresHandlers.UpdateDiretor).WithOpenApi();
+        app.MapPut("/diretores", DiretoresHandlers.UpdateDiretorAsync).WithOpenApi();
 
-        app.MapDelete("/diretores", DiretoresHandlers.DeleteDiretor).WithOpenApi();
+        app.MapDelete("/diretores", DiretoresHandlers.DeleteDiretorAsync).WithOpenApi();
     }
 }
